4509005da32adb266aa654967791ef3e2df28cb9
Aaron <awitthohn@yahoo.com>
2023-07-19 22:40:21 -0400

    added view all employee and all add functions

--- queries.js
+++ queries.js
@@ -29,8 +29,79 @@ function viewAllDepartments() {
     });
   }
 
+  function viewAllEmployees() {
+    const connection = createConnection();
+  
+    connection.query('SELECT * FROM employee', (err, results) => {
+      if (err) {
+        console.error('Error retrieving employees:', err);
+        return;
+      }
+  
+      console.table(results);
+      connection.end();
+    });
+  }
+
+  function addDepartment(name) {
+    const connection = createConnection();
+  
+    connection.query('INSERT INTO department (name) VALUES (?)', [name], err => {
+      if (err) {
+        console.error('Error adding department:', err);
+        return;
+      }
+  
+      console.log('Department added successfully!');
+      connection.end();
+    });
+  }
+
+  function addRole(title, salary, departmentId) {
+    const connection = createConnection();
+  
+    connection.query(
+      'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
+      [title, salary, departmentId],
+      err => {
+        if (err) {
+          console.error('Error adding role:', err);
+          return;
+        }
+  
+        console.log('Role added successfully!');
+        connection.end();
+      }
+    );
+  }
+
+  function addEmployee(firstName, lastName, roleId, managerId) {
+    const connection = createConnection();
+  
+    connection.query(
+      'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+      [firstName, lastName, roleId, managerId],
+      err => {
+        if (err) {
+          console.error('Error adding employee:', err);
+          return;
+        }
+  
+        console.log('Employee added successfully!');
+        connection.end();
+      }
+    );
+  }
+
+
+
 
   module.exports = {
     viewAllDepartments,
     viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+
   };
